be8b7414818488a7c3866251ea7bdd8ec5ba16f1
Evelyn <511788087@qq.com>
2015-01-16 10:09:14 +0800

    ************

---
+++ bubblesort.c
@@ -0,0 +1,26 @@
+#include <iostream.h>
+//冒泡排序算法
+void BubbleSort(int* pData,int Count)
+{ 
+int iTemp; 
+for(int i=1;i<Count;i ) 
+{ 
+for(int j=Count-1;j>=i;j--) 
+{ 
+if(pData[j]<pData[j-1]) 
+{ 
+iTemp = pData[j-1]; 
+pData[j-1] = pData[j]; 
+pData[j] = iTemp; 
+} 
+} 
+} 
+} 
+void main() 
+{ 
+int data[] = {10,9,8,7,6,5,4}; //定义数组
+BubbleSort(data,7); 
+for (int i=0;i<7;i ) 
+cout<<data<<" "; 
+cout<<"\n"; 
+}
